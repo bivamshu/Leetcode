e54f7288c9f77508b80972b338bc63db9f6ef702
bivamshu <bivamshu.khadka86@gmail.com>
2026-06-03 13:23:52 +0545

    feat: two Sum hashmap solution

--- data_structures/hashtable/hashtable.h
+++ data_structures/hashtable/hashtable.h
@@ -1,21 +1,15 @@
-#ifndef HASHTABLE_H //this is called a header guard. It ensures that the same code doesn't get read or copied into your program twice.
-//#ifndef means "if not defined" HASH_TABLE is a uniqiue name
-
+#ifndef HASHTABLE_H
 #define HASHTABLE_H
-//#define means define it right now 
-
-//How this works
-//hashtable.h defines the struch HashTable
-//If two files import the header and the main file compiles, the compiler will see the HashTable declared twice and throw an error. 
-//When the compilers reads the header file for the first time, HASHTABLE_H doesn't exist yet. When the compiler reads the file for the second time, it remembers that the struct has already been defined so it skips directly to the #endif line. 
 
 #include <stdbool.h>
 
+// 1. Make sure this typedef exists
 typedef struct HashTable HashTable;
 
-HashTable* htcreate(int captacity);
+// 2. Double-check the exact spelling of these function signatures
+HashTable* ht_create(int capacity);
 void ht_insert(HashTable* ht, int key, void* value);
 void* ht_search(HashTable* ht, int key);
 void ht_free(HashTable* ht);
 
-#endif //end of the conditional block. 
+#endif
--- problems/1.TwoSum.c
+++ problems/1.TwoSum.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdint.h>
+#include "../data_structures/hashtable/hashtable.h"
 
 int* two_sum(int* nums, int numsSize, int target, int* returnSize){
-
+    //Brute Force method 
     for(int i = 0; i < numsSize; i++){
         for(int j = i + 1; j < numsSize; j++){
             if(nums[i] + nums[j] == target){
@@ -19,13 +21,50 @@ int* two_sum(int* nums, int numsSize, int target, int* returnSize){
     return NULL;
 }
 
+int* twoSumUsingHasmaps(int* nums, int numsSize, int target, int* returnSize){
+    HashTable* ht = ht_create(numsSize);
+
+    int* result = (int*)malloc(sizeof(int) * 2);
+    *returnSize = 0;
+
+    for(int i = 0; i < numsSize; i++){
+        int complement = target - nums[i];
+
+        //Search the table for the complement
+        void* found_index = ht_search(ht, complement);
+
+        if(found_index != NULL){
+            //Convert eh generic pointer back to an index
+            result[0] = (int)(uintptr_t)found_index - 1; //Offset used to safely detect 0 index
+            result[1] = i;
+            *returnSize = 2;
+
+            ht_free(ht);
+            return result;
+        }
+        ht_insert(ht, nums[i], (void*)(uintptr_t)(i + 1));
+
+    }
+    ht_free(ht);
+    free(result);
+    return NULL;
+}
 
 int main(){
-    int array[] = [2, 7, 11, 15];
+    int array[] = {2, 7, 11, 15};
     int arraySize = 4;
     int target = 9;
     int sizeOfResult = 0;
 
+    int* indices = twoSumUsingHasmaps(array, arraySize, target, &sizeOfResult);
+
+    if(indices != NULL){
+        printf("Indices are: %d and %d\n", indices[0], indices[1]);
+        free(indices);
+    }
+    else{
+        printf("No solution found\n");
+    }
     
-    printf("Indices are: %d and %d", two_sum(array, arraySize, target));
+    return 0;
 }
